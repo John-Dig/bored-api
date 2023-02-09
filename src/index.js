7cb71f2b0d9fbe322a0b4155d71df3caec868e1e
John Diggins <rohnfiggins@gmail.com>
2023-02-09 14:34:38 -0800

    works with moving giphy
    Co-authored-by: Chris Johnedis <christopher.johnedis@gmail.com>

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ async function handleOutput() {
   document.getElementById("output").innerText = theActivity;
   const giphyObject = await thingToDo.getGiphy(theActivity);
   const giphyImageURL = giphyObject.data[0].images.downsized_medium.url;
-  const theImageHTML = "<img src =' " + giphyImageURL + "' alt='the Giphy'>";
-  console.log(giphyObject);
-  console.log(giphyImageURL);
+  const theImageHTML = `<img src =${giphyImageURL}alt='the Giphy'>`;
   document.getElementById("giphy-output").innerHTML = theImageHTML;
 
 }
